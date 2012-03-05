efe64bb450213e594364e0c5bec26fb0969622a4
Erik Johansson <erik@ejohansson.se>
2012-03-05 20:00:43 +0100

    Add tellstick constants

---
+++ telldus/constants.py
@@ -0,0 +1,70 @@
+# Copyright (c) 2012 Erik Johansson <erik@ejohansson.se>
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+# USA
+
+# Device methods
+TELLSTICK_TURNON = 1
+TELLSTICK_TURNOFF = 2
+TELLSTICK_BELL = 4
+TELLSTICK_TOGGLE = 8
+TELLSTICK_DIM = 16
+TELLSTICK_LEARN = 32
+TELLSTICK_EXECUTE = 64
+TELLSTICK_UP = 128
+TELLSTICK_DOWN = 256
+TELLSTICK_STOP = 512
+
+# Sensor value types
+TELLSTICK_TEMPERATURE = 1
+TELLSTICK_HUMIDITY = 2
+
+# Error codes
+TELLSTICK_SUCCESS = 0
+TELLSTICK_ERROR_NOT_FOUND = -1
+TELLSTICK_ERROR_PERMISSION_DENIED = -2
+TELLSTICK_ERROR_DEVICE_NOT_FOUND = -3
+TELLSTICK_ERROR_METHOD_NOT_SUPPORTED = -4
+TELLSTICK_ERROR_COMMUNICATION = -5
+TELLSTICK_ERROR_CONNECTING_SERVICE = -6
+TELLSTICK_ERROR_UNKNOWN_RESPONSE = -7
+TELLSTICK_ERROR_SYNTAX = -8
+TELLSTICK_ERROR_BROKEN_PIPE = -9
+TELLSTICK_ERROR_COMMUNICATING_SERVICE = -10
+TELLSTICK_ERROR_UNKNOWN = -99
+
+# Device types
+TELLSTICK_TYPE_DEVICE = 1
+TELLSTICK_TYPE_GROUP = 2
+TELLSTICK_TYPE_SCENE = 3
+
+# Controller types
+TELLSTICK_CONTROLLER_TELLSTICK = 1
+TELLSTICK_CONTROLLER_TELLSTICK_DUO = 2
+TELLSTICK_CONTROLLER_TELLSTICK_NET = 3
+
+# Device changes
+TELLSTICK_DEVICE_ADDED = 1
+TELLSTICK_DEVICE_CHANGED = 2
+TELLSTICK_DEVICE_REMOVED = 3
+TELLSTICK_DEVICE_STATE_CHANGED = 4
+
+# Change types
+TELLSTICK_CHANGE_NAME = 1
+TELLSTICK_CHANGE_PROTOCOL = 2
+TELLSTICK_CHANGE_MODEL = 3
+TELLSTICK_CHANGE_METHOD = 4
+TELLSTICK_CHANGE_AVAILABLE = 5
+TELLSTICK_CHANGE_FIRMWARE = 6
