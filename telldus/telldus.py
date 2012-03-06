e8733c502c6b6d365bd8eaeb8774d1b47d997a44
Erik Johansson <erik@ejohansson.se>
2012-03-06 20:01:11 +0100

    Slightly more py3 compliant

--- telldus/telldus.py
+++ telldus/telldus.py
@@ -15,7 +15,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
 # USA
 
-from library import *
+from .library import *
 
 class TelldusCore(object):
     def __init__(self):
@@ -36,7 +36,7 @@ class TelldusCore(object):
         device.protocol = protocol
         if model is not None:
             device.model = model
-        for key, value in parameters.iteritems():
+        for key, value in parameters.items():
             device.set_parameter(key, value)
         return device
 
