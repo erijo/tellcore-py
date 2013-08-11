ae11e1fe9dd600f22bef068f5a9ebad0dac54b2a
Erik Johansson <erik@ejohansson.se>
2013-08-11 20:31:32 +0200

    Update copyright year to include 2013

--- test/test_library.py
+++ test/test_library.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2012 Erik Johansson <erik@ejohansson.se>
+# Copyright (c) 2012-2013 Erik Johansson <erik@ejohansson.se>
 # 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
