erijo/tellcore-py

commit d3a821c98dc5b3894862f170ba8acc04e0bb8b07

Retain traceback after remove if add_device fails

tellcore/telldus.py
@@ -150,12 +150,14 @@ class TelldusCore(object):
             for key, value in parameters.items():
                 device.set_parameter(key, value)
             return device
-        except Exception as e:
+        except Exception:
+            import sys
+            exc_info = sys.exc_info()
             try:
                 device.remove()
             except:
                 pass
-            raise e
+            raise exc_info[0], exc_info[1], exc_info[2]
 
     def send_raw_command(self, command, reserved=0):
         return self.lib.tdSendRawCommand(command, reserved)

commit 09db5046e9fc130a8311dd1fbdda0cfcea39a1f8

Fix python3 error introduced in d3a821c9

tellcore/telldus.py
@@ -157,7 +157,11 @@ class TelldusCore(object):
                 device.remove()
             except:
                 pass
-            raise exc_info[0], exc_info[1], exc_info[2]
+
+            if "with_traceback" in dir(Exception):
+                raise exc_info[0].with_traceback(exc_info[1], exc_info[2])
+            else:
+                exec("raise exc_info[0], exc_info[1], exc_info[2]")
 
     def send_raw_command(self, command, reserved=0):
         return self.lib.tdSendRawCommand(command, reserved)