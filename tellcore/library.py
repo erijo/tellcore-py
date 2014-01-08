erijo/tellcore-py

commit 2fe9a71ea80d57829340c8179d2804292fb72fe0

Use find_library to locate TelldusCore on OS X
Only fall back to hard coded value if find_library fails.

tellcore/library.py
@@ -28,8 +28,10 @@ if platform.system() == 'Windows':
     LIBRARY_NAME = 'TelldusCore.dll'
 else:
     from ctypes import CFUNCTYPE as FUNCTYPE, cdll as DllLoader
-    if platform.system() == 'Darwin':
-        LIBRARY_NAME = '/Library/Frameworks/TelldusCore.framework/TelldusCore'
+    if platform.system() != 'Darwin':
+        from ctypes.util import find_library
+        LIBRARY_NAME = find_library('TelldusCore') or \
+            '/Library/Frameworks/TelldusCore.framework/TelldusCore'
     else:
         LIBRARY_NAME = 'libtelldus-core.so.2'
 

commit 494b75c7fee3732fac146c361b3adc38c5963af7

Fix typo introduced in 2fe9a71e

tellcore/library.py
@@ -28,7 +28,7 @@ if platform.system() == 'Windows':
     LIBRARY_NAME = 'TelldusCore.dll'
 else:
     from ctypes import CFUNCTYPE as FUNCTYPE, cdll as DllLoader
-    if platform.system() != 'Darwin':
+    if platform.system() == 'Darwin':
         from ctypes.util import find_library
         LIBRARY_NAME = find_library('TelldusCore') or \
             '/Library/Frameworks/TelldusCore.framework/TelldusCore'