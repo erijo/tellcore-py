975545a4bd1206f3c809a87084859eb33337a419
Erik Johansson <erik@ejohansson.se>
2014-01-09 21:21:14 +0100

    Prepare for v1.0.0

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ from distutils.core import setup
 
 setup(
     name='tellcore-py',
-    version='0.9.0',
+    version='1.0.0',
     author='Erik Johansson',
     author_email='erik@ejohansson.se',
     packages=['tellcore'],
     url='https://github.com/erijo/tellcore-py',
-    license='GPLv3',
+    license='GPLv3+',
     description='Python wrapper for Telldus\' home automation library',
     long_description=open('README.rst').read(),
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
