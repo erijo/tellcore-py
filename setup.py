7a49b0470a0615882aadbc332fef445e8d8110c2
Erik Johansson <erik@ejohansson.se>
2014-01-26 20:22:07 +0100

    Prepare for v1.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='tellcore-py',
-    version='1.0.0',
+    version='1.0.1',
     author='Erik Johansson',
     author_email='erik@ejohansson.se',
     packages=['tellcore'],
