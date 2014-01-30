e68852871007594298734d1d9cefe751065afecd
Erik Johansson <erik@ejohansson.se>
2014-01-30 14:35:23 +0100

    Python 2.6 is not supported

--- setup.py
+++ setup.py
@@ -35,7 +35,6 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.0',
